0042372db3eeb12f47b7de47fb7b823e61bd18aa
FreashHead <UniverAlex@ya.ru>
2018-07-03 08:06:53 +0300

    if applied this commit will add another space into inline comment

--- src/transliterator/gui/controller.py
+++ src/transliterator/gui/controller.py
@@ -2,7 +2,7 @@ from src.transliterator.transliterator import transliterate_symbol
 
 
 def on_start(source_text, messages_var):
-    source = source_text.get('1.0', 'end')[:-1] # [:-1] is removing last '\n' added from Text class
+    source = source_text.get('1.0', 'end')[:-1]  # [:-1] is removing last '\n' added from Text class
     messages = []
     for symbol in source:
         token = transliterate_symbol(symbol)
